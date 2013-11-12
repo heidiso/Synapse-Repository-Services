d7578376fcb02720a0d778986e6300f9d82419be
Heidi <heidi.so@sagebase.org>
2013-11-12 15:45:13 -0800

    Take out unnecessary imports...

--- tools/tool-migration-utility/src/main/java/org/sagebionetworks/tool/migration/v3/MigrationClient.java
+++ tools/tool-migration-utility/src/main/java/org/sagebionetworks/tool/migration/v3/MigrationClient.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
